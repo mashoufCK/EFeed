00e334aad5ae7c7d757be64ddd7e951d7044ce53
MC <mashuf.ir@gmail.com>
2023-05-22 10:29:10 -0500

    Move test logic to a separate type "HTTPClientSpt"

--- EssenFeedTests/REmoteFeedLoaderTests.swift
+++ EssenFeedTests/REmoteFeedLoaderTests.swift
@@ -10,25 +10,36 @@ import XCTest
 class RemoteFeedLoader  {
     
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
- 
-    static let shared = HTTPClient()
+ //not a singleton anymore, by making this a var we can  make subclasses of this
+    static var shared = HTTPClient()
     
     private init () {}
+    func get(from url: URL) {
+      }
     
+}
+
+
+class HTTPClientSpy: HTTPClient {
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     var requestedURL : URL?
 }
+
 
 final class REmoteFeedLoaderTests: XCTestCase {
 
     func test_init() {
         
-        let client = HTTPClient.shared
-        
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _  = RemoteFeedLoader()
         
         //sut.load()
@@ -38,7 +49,8 @@ final class REmoteFeedLoaderTests: XCTestCase {
     }
     
     func test_load_requestDataFromURL () {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         
         sut.load()
